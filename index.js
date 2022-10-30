acd63d8deb6a52eb82662c268ec15af23659da66
merkbilgogie <william.miller5@ontariotechu.net>
2022-10-30 06:17:28 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+
+app.get('/', (req, res) =>{
+  res.render("home", {
+    
+  })
+
+})
+
+app.get('/home', (req, res) => {
+  res.render("home")
+
+})
+
+app.get('/about', (req, res) =>{
+  res.render("about")
+  
+})
+app.get('/projects', (req, res) =>{
+  res.render("projects")
+  
+})
+app.get('/blogs', (req, res) =>{
+  res.render("blogs")
+  
+})
+
+
+
+
+app.listen(2000)
+
